5b949646a368031c8464d2f581a63026b1c0056a
Kota Suzuki <addressuzuki@gmail.com>
2024-03-04 20:33:16 +0900

    Initial Commit

---
+++ ChatView/ChatViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatViewApp.swift
+//  ChatView
+//
+//  Created by 鈴木航太 on 2024/03/04.
+//
+
+import SwiftUI
+
+@main
+struct ChatViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
